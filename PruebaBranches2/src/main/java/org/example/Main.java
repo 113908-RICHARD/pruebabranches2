22abc14b9f29011b553b3c406265b0a16c5d9b8d
113908-RICHARD <128639135+113908-RICHARD@users.noreply.github.com>
2023-05-11 12:44:37 -0300

    update feature1

--- PruebaBranches2/src/main/java/org/example/Main.java
+++ PruebaBranches2/src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Creacion branch feature1");
         System.out.println("merge develop feature1");
+        System.out.println("merge develop 2");
     }
 }
